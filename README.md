029566e3495e34bd0469dae6314f6c1ad85cd49b
muhammaduzair19 <muhammaduzairilyas@gmail.com>
2024-11-05 11:21:07 +0500

    project intialisation

---
+++ README.md
@@ -0,0 +1,7 @@
+# YOBBO
+
+![YOBBO LOGO](/public/logo-yobbo.png)
+
+A landing page for NFT Character known as Yobbo based on Ethereum blockchain.
+
+
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div className="font-extrabold text-5xl bg-gradient-to-br from-blue-600 via-white to-blue-400 bg-clip-text tracking-tighter text-transparent ">
+            YOBOO
+        </div>
+    );
+};
+
+export default App;
